9a5bf31ddfd98181f1e8dcbaec8ce82ec382e612
benoz23 <gianluca.benotto@gmail.com>
2023-11-01 21:13:47 +0100

    Update prevnext.js

--- prevnext.js
+++ prevnext.js
@@ -1,32 +1,14 @@
-var marker = document.querySelector("a-marker");
-var entityList = marker.querySelectorAll("a-entity:not([camera])"); // Exclude entities with the "camera" attribute
-var currentEntityIndex = 0;
-var prevButton = document.getElementById("prev-button");
-var nextButton = document.getElementById("next-button");
+var currentChildIndex = 0;
+var children = document.querySelector('#marker-1').children;
 
-// Initialize, show the first entity, and hide the rest
-entityList.forEach(function (entity, index) {
-    if (index === currentEntityIndex) {
-        entity.setAttribute("visible", true);
-    } else {
-        entity.setAttribute("visible", false);
+function changeChild(offset) {
+    children[currentChildIndex].setAttribute('visible', 'false');
+    currentChildIndex += offset;
+    if (currentChildIndex < 0) {
+        currentChildIndex = 0;
     }
-});
-
-// Show the next entity
-nextButton.addEventListener("click", function () {
-    if (currentEntityIndex < entityList.length - 1) {
-        entityList[currentEntityIndex].setAttribute("visible", false);
-        currentEntityIndex++;
-        entityList[currentEntityIndex].setAttribute("visible", true);
+    if (currentChildIndex >= children.length) {
+        currentChildIndex = children.length - 1;
     }
-});
-
-// Show the previous entity
-prevButton.addEventListener("click", function () {
-    if (currentEntityIndex > 0) {
-        entityList[currentEntityIndex].setAttribute("visible", false);
-        currentEntityIndex--;
-        entityList[currentEntityIndex].setAttribute("visible", true);
-    }
-});
+    children[currentChildIndex].setAttribute('visible', 'true');
+}
