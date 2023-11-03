2bff7445b8b6c8794dac3d6fa3170b7a5d74d3fc
benoz23 <gianluca.benotto@gmail.com>
2023-11-03 23:05:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-window.addEventListener('orientationchange', function () {
-    location.reload();
-  });
+// JavaScript
+const container = document.getElementById('container');
+
+// Funzione per animare il container
+function animateContainer(toPosition) {
+    container.setAttribute('animation__001', {
+        property: 'position',
+        to: toPosition,
+        startEvents: 'click_next'
+    });
+    container.emit('click_next', null, false);
+}
+
+// Gestione dei pulsanti "prev" e "next"
+function prev() {
+    const toPosition = { x: -0.4, y: 1.525, z: -0.15 };
+    animateContainer(toPosition);
+}
+
+function next() {
+    const toPosition = { x: +0.4, y: 1.525, z: -0.15 };
+    animateContainer(toPosition);
+}
