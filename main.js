4a4a9ef855887a9204b45267d02b6d6cbf9eb01c
benoz23 <gianluca.benotto@gmail.com>
2023-11-04 21:54:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
+// Get the camera element
+const camera = document.querySelector('#camera');
+
 document.getElementById('prev-button').onclick = function() {
-    document.querySelector('#camera').setAttribute('animation__prev', {'property': 'position',
-                                                                        'to': {x: parseFloat(currentPosition.x) - 0.25, y: currentPosition.y, z: currentPosition.z},                                 
-                                                                        'startEvents': 'prevButtonClick');
-    document.querySelector('#camera').emit('prevButtonClick');
+    // Get the current position of the camera
+    const currentPosition = camera.getAttribute('position');
+    
+    // Calculate the new position as an increment from the current position
+    const newPosition = {
+        x: parseFloat(currentPosition.x) - 0.25,
+        y: currentPosition.y,
+        z: currentPosition.z
+    };
+
+    // Set the animation attributes
+    camera.setAttribute('animation__prev', {
+        property: 'position',
+        to: newPosition,
+        startEvents: 'prevButtonClick'
+    });
+    
+    // Trigger the animation
+    camera.emit('prevButtonClick');
 };
 
 document.getElementById('next-button').onclick = function() {
-    document.querySelector('#camera').setAttribute('animation__next', {'property': 'position',
-                                                                        'to': {x: parseFloat(currentPosition.x) + 0.25, y: currentPosition.y, z: currentPosition.z},                                 
-                                                                        'startEvents': 'nextButtonClick');
-    document.querySelector('#camera').emit('nextButtonClick');
+    // Get the current position of the camera
+    const currentPosition = camera.getAttribute('position');
+    
+    // Calculate the new position as an increment from the current position
+    const newPosition = {
+        x: parseFloat(currentPosition.x) + 0.25,
+        y: currentPosition.y,
+        z: currentPosition.z
+    };
+
+    // Set the animation attributes
+    camera.setAttribute('animation__next', {
+        property: 'position',
+        to: newPosition,
+        startEvents: 'nextButtonClick'
+    });
+    
+    // Trigger the animation
+    camera.emit('nextButtonClick');
 };
