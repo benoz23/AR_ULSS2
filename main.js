191c02944b0683fe4942c73913576f23cebe5370
benoz23 <gianluca.benotto@gmail.com>
2023-11-04 21:50:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 document.getElementById('prev-button').onclick = function() {
+    document.querySelector('#camera').setAttribute('animation__prev', {'property': 'position',
+                                                                        'to': {x: parseFloat(currentPosition.x) - 0.25, y: currentPosition.y, z: currentPosition.z},                                 
+                                                                        'startEvents': 'prevButtonClick');
     document.querySelector('#camera').emit('prevButtonClick');
 };
 
 document.getElementById('next-button').onclick = function() {
+    document.querySelector('#camera').setAttribute('animation__next', {'property': 'position',
+                                                                        'to': {x: parseFloat(currentPosition.x) + 0.25, y: currentPosition.y, z: currentPosition.z},                                 
+                                                                        'startEvents': 'nextButtonClick');
     document.querySelector('#camera').emit('nextButtonClick');
 };
