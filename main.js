d6dd2b8c144ec979fdedb6e01097d71e93bf4b7d
benoz23 <gianluca.benotto@gmail.com>
2023-11-05 06:22:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,79 +4,79 @@ const camera = document.querySelector('#camera');
 document.getElementById('prev-button').onclick = function() {
     // Get the current position of the camera
     const currentPosition = camera.getAttribute('position');
+    
+    // Get all container children
+    const children = document.querySelectorAll('.container-child');
 
-    // Calculate the new position as an increment from the current position
-    const newPosition = {
-        x: parseFloat(currentPosition.x) - 0.25,
-        y: currentPosition.y,
-        z: currentPosition.z
-    };
+    // Find the active child
+    const activeChild = document.querySelector('.container-child[active-child]');
 
-    // Get the boundaries for the container
-    const container = document.querySelector('#container');
-    const containerChildren = container.querySelectorAll('.container-child');
+    // Find the index of the active child
+    const activeChildIndex = Array.from(children).indexOf(activeChild);
 
-    // Check if there are any children with the class name
-    if (containerChildren.length > 0) {
-        // Get the first child (if any)
-        const firstChild = containerChildren[0];
-        const containerX = parseFloat(container.getAttribute('position').x);
-        const firstChildX = parseFloat(firstChild.getAttribute('position').x);
-        const firstChildWidth = parseFloat(firstChild.getAttribute('width'));
+    // Check if there is a previous child
+    if (activeChildIndex > 0) {
+        // Calculate the new position for the camera
+        const newPosition = {
+            x: parseFloat(currentPosition.x) - 0.175,
+            y: currentPosition.y,
+            z: currentPosition.z
+        };
 
-        // Check if the new position exceeds the beginning of the container
-        if (newPosition.x < containerX + firstChildX - firstChildWidth / 2) {
-            newPosition.x = containerX + firstChildX - firstChildWidth / 2;
-        }
+        // Set the new camera position
+        camera.setAttribute('animation__prev', {
+            property: 'position',
+            to: newPosition,
+            startEvents: 'prevButtonClick'
+        });
+
+        // Remove the "active-child" attribute from the current active child
+        activeChild.removeAttribute('active-child');
+
+        // Set the "active-child" attribute to the previous child
+        children[activeChildIndex - 1].setAttribute('active-child', '');
+
+        // Trigger the animation
+        camera.emit('prevButtonClick');
     }
-
-    // Set the animation attributes
-    camera.setAttribute('animation__prev', {
-        property: 'position',
-        to: newPosition,
-        startEvents: 'prevButtonClick'
-    });
-
-    // Trigger the animation
-    camera.emit('prevButtonClick');
 };
 
 document.getElementById('next-button').onclick = function() {
     // Get the current position of the camera
     const currentPosition = camera.getAttribute('position');
+    
+    // Get all container children
+    const children = document.querySelectorAll('.container-child');
 
-    // Calculate the new position as an increment from the current position
-    const newPosition = {
-        x: parseFloat(currentPosition.x) + 0.25,
-        y: currentPosition.y,
-        z: currentPosition.z
-    };
+    // Find the active child
+    const activeChild = document.querySelector('.container-child[active-child]');
 
-    // Get the boundaries for the container
-    const container = document.querySelector('#container');
-    const containerChildren = container.querySelectorAll('.container-child');
+    // Find the index of the active child
+    const activeChildIndex = Array.from(children).indexOf(activeChild);
 
-    // Check if there are any children with the class name
-    if (containerChildren.length > 0) {
-        // Get the last child (if any)
-        const lastChild = containerChildren[containerChildren.length - 1];
-        const containerX = parseFloat(container.getAttribute('position').x);
-        const lastChildX = parseFloat(lastChild.getAttribute('position').x);
-        const lastChildWidth = parseFloat(lastChild.getAttribute('width'));
+    // Check if there is a next child
+    if (activeChildIndex < children.length - 1) {
+        // Calculate the new position for the camera
+        const newPosition = {
+            x: parseFloat(currentPosition.x) + 0.175,
+            y: currentPosition.y,
+            z: currentPosition.z
+        };
 
-        // Check if the new position exceeds the end of the container
-        if (newPosition.x > containerX + lastChildX + lastChildWidth / 2) {
-            newPosition.x = containerX + lastChildX + lastChildWidth / 2;
-        }
+        // Set the new camera position
+        camera.setAttribute('animation__next', {
+            property: 'position',
+            to: newPosition,
+            startEvents: 'nextButtonClick'
+        });
+
+        // Remove the "active-child" attribute from the current active child
+        activeChild.removeAttribute('active-child');
+
+        // Set the "active-child" attribute to the next child
+        children[activeChildIndex + 1].setAttribute('active-child', '');
+
+        // Trigger the animation
+        camera.emit('nextButtonClick');
     }
-
-    // Set the animation attributes
-    camera.setAttribute('animation__next', {
-        property: 'position',
-        to: newPosition,
-        startEvents: 'nextButtonClick'
-    });
-
-    // Trigger the animation
-    camera.emit('nextButtonClick');
 };
