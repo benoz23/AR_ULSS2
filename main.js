4aef02ced32b2400c4f27ab26e49267773b07a44
benoz23 <gianluca.benotto@gmail.com>
2023-11-05 15:46:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,37 @@ const camera = document.querySelector('#camera');
 // Get all container children
 const containerChildren = container.querySelectorAll('.container-child');
 
+// Check for children with width > height
+for (const child of containerChildren) {
+    const childWidth = child.offsetWidth;
+    const childHeight = child.offsetHeight;
+
+    if (childWidth > childHeight) {
+        // Create two auxiliary entities
+        const aux1 = document.createElement('a-entity');
+        const aux2 = document.createElement('a-entity');
+
+        // Set their class to "container-child"
+        aux1.classList.add('container-child');
+        aux2.classList.add('container-child');
+
+        // Determine the auxiliary entities' positions
+        const aux1X = child.getAttribute('position').x - (childWidth / 6);
+        const aux2X = child.getAttribute('position').x + (childWidth / 6);
+        const auxY = child.getAttribute('position').y;
+        const auxZ = parseFloat(child.getAttribute('position').z) + 0.175;
+
+        // Set the auxiliary entities' positions
+        aux1.setAttribute('position', `${aux1X} ${auxY} ${auxZ}`);
+        aux2.setAttribute('position', `${aux2X} ${auxY} ${auxZ}`);
+
+        // Insert the auxiliary entities after the original child
+        container.insertBefore(aux1, child.nextSibling);
+        container.insertBefore(aux2, child.nextSibling);
+    }
+}
+
+// Previous button click event handler
 document.getElementById('prev-button').onclick = function() {
     // Find the active child
     const activeChild = container.querySelector('[active-child]');
@@ -28,12 +59,13 @@ document.getElementById('prev-button').onclick = function() {
 
         // Set the "active-child" attribute on the previous child
         prevChild.setAttribute('active-child', '');
-        
+
         // Trigger the animation
         camera.emit('prevButtonClick');
     }
 };
 
+// Next button click event handler
 document.getElementById('next-button').onclick = function() {
     // Find the active child
     const activeChild = container.querySelector('[active-child]');
@@ -57,7 +89,7 @@ document.getElementById('next-button').onclick = function() {
 
         // Set the "active-child" attribute on the next child
         nextChild.setAttribute('active-child', '');
-        
+
         // Trigger the animation
         camera.emit('nextButtonClick');
     }
