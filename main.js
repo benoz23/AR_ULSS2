f1279d8446b19d5fb6d15c0d681799fa9dfccb5b
benoz23 <gianluca.benotto@gmail.com>
2023-11-05 06:28:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,81 +1,63 @@
-// Get the camera element
+// Get the container and camera elements
+const container = document.querySelector('#container');
 const camera = document.querySelector('#camera');
 
+// Get all container children
+const containerChildren = container.querySelectorAll('.container-child');
+
 document.getElementById('prev-button').onclick = function() {
-    // Get the current position of the camera
-    const currentPosition = camera.getAttribute('position');
-    
-    // Get all container children
-    const children = document.querySelectorAll('.container-child');
-
     // Find the active child
-    const activeChild = document.querySelector('.container-child[active-child]');
+    const activeChild = container.querySelector('[active-child]');
 
-    // Find the index of the active child
-    const activeChildIndex = Array.from(children).indexOf(activeChild);
+    // Find the previous child based on the order of children
+    const prevIndex = Array.from(containerChildren).indexOf(activeChild) - 1;
 
-    // Check if there is a previous child
-    if (activeChildIndex > 0) {
-        // Calculate the new position for the camera
-        const newPosition = {
-            x: parseFloat(currentPosition.x) - 0.175,
-            y: currentPosition.y,
-            z: currentPosition.z
-        };
+    if (prevIndex >= 0) {
+        const prevChild = containerChildren[prevIndex];
+        const prevPosition = prevChild.getAttribute('position');
 
-        // Set the new camera position
+        // Move the camera to the previous child's position
         camera.setAttribute('animation__prev', {
             property: 'position',
-            to: newPosition,
+            to: `${prevPosition.x} ${prevPosition.y} ${parseFloat(prevPosition.z) - 0.175}`,
             startEvents: 'prevButtonClick'
         });
 
         // Remove the "active-child" attribute from the current active child
         activeChild.removeAttribute('active-child');
 
-        // Set the "active-child" attribute to the previous child
-        children[activeChildIndex - 1].setAttribute('active-child', '');
-
+        // Set the "active-child" attribute on the previous child
+        prevChild.setAttribute('active-child', '');
+        
         // Trigger the animation
         camera.emit('prevButtonClick');
     }
 };
 
 document.getElementById('next-button').onclick = function() {
-    // Get the current position of the camera
-    const currentPosition = camera.getAttribute('position');
-    
-    // Get all container children
-    const children = document.querySelectorAll('.container-child');
-
     // Find the active child
-    const activeChild = document.querySelector('.container-child[active-child]');
+    const activeChild = container.querySelector('[active-child]');
 
-    // Find the index of the active child
-    const activeChildIndex = Array.from(children).indexOf(activeChild);
+    // Find the next child based on the order of children
+    const nextIndex = Array.from(containerChildren).indexOf(activeChild) + 1;
 
-    // Check if there is a next child
-    if (activeChildIndex < children.length - 1) {
-        // Calculate the new position for the camera
-        const newPosition = {
-            x: parseFloat(currentPosition.x) + 0.175,
-            y: currentPosition.y,
-            z: currentPosition.z
-        };
+    if (nextIndex < containerChildren.length) {
+        const nextChild = containerChildren[nextIndex];
+        const nextPosition = nextChild.getAttribute('position');
 
-        // Set the new camera position
+        // Move the camera to the next child's position
         camera.setAttribute('animation__next', {
             property: 'position',
-            to: newPosition,
+            to: `${nextPosition.x} ${nextPosition.y} ${parseFloat(nextPosition.z) - 0.175}`,
             startEvents: 'nextButtonClick'
         });
 
         // Remove the "active-child" attribute from the current active child
         activeChild.removeAttribute('active-child');
 
-        // Set the "active-child" attribute to the next child
-        children[activeChildIndex + 1].setAttribute('active-child', '');
-
+        // Set the "active-child" attribute on the next child
+        nextChild.setAttribute('active-child', '');
+        
         // Trigger the animation
         camera.emit('nextButtonClick');
     }
