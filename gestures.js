0db4634fb456d6d110cae9745acb01e24ab0eb5f
benoz23 <gianluca.benotto@gmail.com>
2023-11-07 15:29:47 +0100

    reset pos

--- gestures.js
+++ gestures.js
@@ -1,49 +1,10 @@
 /* global AFRAME, THREE */
 
-AFRAME.registerComponent('position-monitor', {
-  schema: {
-    logInterval: { default: 1000 } // Log position every 1000ms (1 second)
-  },
-
-  init: function () {
-    // Initialize the position log timer.
-    this.positionLogTimer = 0;
-
-    // Get a reference to the camera entity.
-    this.camera = this.el;
-
-    // Store the initial camera position.
-    this.lastPosition = this.camera.object3D.position.clone();
-  },
-
-  tick: function (time, timeDelta) {
-    // Calculate the time elapsed since the last position log.
-    this.positionLogTimer += timeDelta;
-
-    // Check if it's time to log the position.
-    if (this.positionLogTimer >= this.data.logInterval) {
-      // Get the current camera position.
-      const currentPosition = this.camera.object3D.position.clone();
-
-      // Log the x, y, and z positions to the console.
-      console.log('Camera Position - X:', currentPosition.x, 'Y:', currentPosition.y, 'Z:', currentPosition.z);
-
-      // Reset the position log timer.
-      this.positionLogTimer = 0;
-
-      // Update the last position to the current position for the next log interval.
-      this.lastPosition.copy(currentPosition);
-    }
-  }
-});
-
 AFRAME.registerComponent("gesture-handler", {
   schema: {
     enabled: { default: true },
     movementFactor: { default: 1 },
-    zoomFactor: { default: 1 },
-    min: { type: 'vec3', default: {x: -0.25, y: 1.3, z: 0.1} }, // Minimum position
-    max: { type: 'vec3', default: {x: 1.125, y: 1.4, z: 0.2} }  // Maximum position
+    zoomFactor: { default: 1 }
   },
 
   init: function () {
@@ -82,44 +43,22 @@ AFRAME.registerComponent("gesture-handler", {
   },
 
   handleMovementHor: function (event) {
-    const newPosition = this.el.object3D.position.clone();
-    newPosition.x -= event.detail.positionChange.x * this.data.movementFactor;
-
-    // Check and enforce movement boundaries relative to the container.
-    newPosition.x = Math.max(this.data.min.x, Math.min(this.data.max.x, newPosition.x));
-    newPosition.y = Math.max(this.data.min.y, Math.min(this.data.max.y, newPosition.y));
-    newPosition.z = Math.max(this.data.min.z, Math.min(this.data.max.z, newPosition.z));
-
-    this.el.object3D.position.copy(newPosition);
+    this.el.object3D.position.x -=
+      event.detail.positionChange.x * this.data.movementFactor;
   },
 
   handleMovementVert: function (event) {
-    const newPosition = this.el.object3D.position.clone();
-    newPosition.y += event.detail.positionChange.y * this.data.movementFactor;
-    newPosition.z -= event.detail.positionChange.y * this.data.movementFactor;
-
-    // Check and enforce movement boundaries relative to the container.
-    newPosition.x = Math.max(this.data.min.x, Math.min(this.data.max.x, newPosition.x));
-    newPosition.y = Math.max(this.data.min.y, Math.min(this.data.max.y, newPosition.y));
-    newPosition.z = Math.max(this.data.min.z, Math.min(this.data.max.z, newPosition.z));
-
-    this.el.object3D.position.copy(newPosition);
+    this.el.object3D.position.y +=
+      event.detail.positionChange.y * this.data.movementFactor;
+    this.el.object3D.position.z -=
+      event.detail.positionChange.y * this.data.movementFactor;
   },
     
   handleZoom: function (event) {
-    const newPosition = this.el.object3D.position.clone();
-    newPosition.y -= event.detail.spreadChange * this.data.zoomFactor;
-    newPosition.z -= event.detail.spreadChange * this.data.zoomFactor;
-
-    // Check and enforce movement boundaries relative to the container.
-    newPosition.x = Math.max(this.data.min.x, Math.min(this.data.max.x, newPosition.x));
-    newPosition.y = Math.max(this.data.min.y, Math.min(this.data.max.y, newPosition.y));
-    newPosition.z = Math.max(this.data.min.z, Math.min(this.data.max.z, newPosition.z));
-
-    this.el.object3D.position.copy(newPosition);
+    this.el.object3D.position.y -= event.detail.spreadChange * this.data.zoomFactor;
+    this.el.object3D.position.z -= event.detail.spreadChange * this.data.zoomFactor;
   }
 });
-
 
 // Component that detects and emits events for touch gestures
 
