2b46aa63de69fd1ea5142c88ad67ad5e64aeb154
benoz23 <gianluca.benotto@gmail.com>
2023-11-09 21:21:25 +0100

    DOMContentLoaded

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-if (typeof AFRAME === 'undefined') {
-    document.getElementById('to-html').style.display = 'block';
-  } else {
-    document.getElementById('to-iframe').style.display = 'block';
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    if (typeof AFRAME === 'undefined') {
+      var toHtmlButton = document.getElementById('to-html');
+      if (toHtmlButton) {
+        toHtmlButton.style.display = 'block';
+      }
+    } else {
+      var toAframeButton = document.getElementById('to-aframe');
+      if (toAframeButton) {
+        toAframeButton.style.display = 'block';
+      }
+    }
+  });
   
