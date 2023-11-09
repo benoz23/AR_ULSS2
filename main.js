ebd449f99fae0e41f487290f3a9b069854238d95
benoz23 <gianluca.benotto@gmail.com>
2023-11-09 21:28:03 +0100

    fixfix

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (typeof AFRAME === 'undefined') {
-      var toHtmlButton = document.getElementById('to-html');
-      if (toHtmlButton) {
-        toHtmlButton.style.display = 'block';
-      }
+        var toHtmlButton = document.getElementById('to-html');
+        if (toHtmlButton) {
+            toHtmlButton.style.display = 'block';
+            console.log("HTML")
+        }
     } else {
-      var toAframeButton = document.getElementById('to-aframe');
-      if (toAframeButton) {
-        toAframeButton.style.display = 'block';
-      }
+        var toAframeButton = document.getElementById('to-aframe');
+        if (toAframeButton) {
+            toAframeButton.style.display = 'block';
+            console.log("A-frame")
+        }
     }
-  });
-  
+});
