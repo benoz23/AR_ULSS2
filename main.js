e497e7e8c7606f3a0c31b2c34abf741f5edf91d9
benoz23 <gianluca.benotto@gmail.com>
2023-11-14 22:24:34 +0100

    js fix

--- main.js
+++ main.js
@@ -1,122 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Check if the Device Motion API is supported
-  const isDeviceMotionSupported = 'DeviceMotionEvent' in window;
-  console.log('Device Motion API Supported:', isDeviceMotionSupported);
-
-  // Function to request motion sensor permission
-  async function requestMotionPermission() {
-    try {
-      // Use polyfill if native method is not available
-      if (!DeviceMotionEvent.requestPermission) {
-        DeviceMotionEvent.requestPermission = function () {
-          console.log('Using polyfill for DeviceMotionEvent.requestPermission()');
-          return Promise.resolve(true);
-        };
+  // Function to check if sensors are present and enabled and A-Frame is supported
+  function isSensorsAndAFrameSupported() {
+      // Check if A-Frame is supported
+      if (!window.AFRAME) {
+          return false;
       }
 
-      // Request permission using native method or polyfill
-      await DeviceMotionEvent.requestPermission();
-      return true;
-    } catch (error) {
-      console.error('Motion sensor permission denied:', error);
-      return false;
-    }
-  }
-
-  // Check if motion sensors are enabled
-  async function checkMotionSensors() {
-    if (isDeviceMotionSupported) {
-      const motionPermission = await requestMotionPermission();
-      console.log('Motion Sensors Enabled:', motionPermission);
-      return motionPermission;
-    } else {
-      console.log('Motion Sensors not supported.');
-      return false;
-    }
-  }
-
-  // Check if the Device Orientation API is supported
-  const isDeviceOrientationSupported = 'DeviceOrientationEvent' in window;
-  console.log('Device Orientation API Supported:', isDeviceOrientationSupported);
-
-  // Function to request orientation sensor permission
-  async function requestOrientationPermission() {
-    try {
-      // Use polyfill if native method is not available
-      if (!DeviceOrientationEvent.requestPermission) {
-        DeviceOrientationEvent.requestPermission = function () {
-          console.log('Using polyfill for DeviceOrientationEvent.requestPermission()');
-          return Promise.resolve(true);
-        };
+      // Check if sensors are present and enabled
+      if (!navigator.permissions || !navigator.permissions.query || !AFRAME.scenes.length) {
+          return false;
       }
 
-      // Request permission using native method or polyfill
-      await DeviceOrientationEvent.requestPermission();
       return true;
-    } catch (error) {
-      console.error('Orientation sensor permission denied:', error);
-      return false;
-    }
   }
-
-  // Check if orientation sensors are enabled
-  async function checkOrientationSensors() {
-    if (isDeviceOrientationSupported) {
-      const orientationPermission = await requestOrientationPermission();
-      console.log('Orientation Sensors Enabled:', orientationPermission);
-      return orientationPermission;
-    } else {
-      console.log('Orientation Sensors not supported.');
-      return false;
-    }
-  }
-
-  // Check if A-Frame is supported
-  const isAFrameSupported = typeof AFRAME !== 'undefined';
-  console.log('A-Frame Supported:', isAFrameSupported);
 
   // Function to apply styles based on conditions
-  function applyStyles(elements, visibilityStyle, displayStyle) {
-    elements.forEach(function (element) {
-      element.style.visibility = visibilityStyle;
-      element.style.display = displayStyle;
-    });
+  function applyStyles() {
+      var elementsToHide = document.querySelectorAll('[yes_af_hide]');
+      var elementsToRemove = document.querySelectorAll('[yes_af_remove]');
+      var elementsNoHide = document.querySelectorAll('[no_af_hide]');
+      var elementsNoRemove = document.querySelectorAll('[no_af_remove]');
+
+      if (isSensorsAndAFrameSupported()) {
+          for (const element of elementsToHide) {
+              element.style.visibility = 'hidden';
+          }
+
+          for (const element of elementsToRemove) {
+              element.style.display = 'none';
+          }
+      } else {
+          for (const element of elementsNoHide) {
+              element.style.visibility = 'hidden';
+          }
+
+          for (const element of elementsNoRemove) {
+              element.style.display = 'none';
+          }
+      }
   }
 
-  // Elements with "yes_af_hide" attribute
-  const yesAfHideElements = document.querySelectorAll('[yes_af_hide]');
-  console.log('Elements with yes_af_hide attribute:', yesAfHideElements);
-
-  // Elements with "yes_af_remove" attribute
-  const yesAfRemoveElements = document.querySelectorAll('[yes_af_remove]');
-  console.log('Elements with yes_af_remove attribute:', yesAfRemoveElements);
-
-  // Elements with "no_af_hide" attribute
-  const noAfHideElements = document.querySelectorAll('[no_af_hide]');
-  console.log('Elements with no_af_hide attribute:', noAfHideElements);
-
-  // Elements with "no_af_remove" attribute
-  const noAfRemoveElements = document.querySelectorAll('[no_af_remove]');
-  console.log('Elements with no_af_remove attribute:', noAfRemoveElements);
-
-  // Check conditions and apply styles accordingly
-  async function checkAndApplyStyles() {
-    const motionSensorsEnabled = await checkMotionSensors();
-    const orientationSensorsEnabled = await checkOrientationSensors();
-
-    if (motionSensorsEnabled && orientationSensorsEnabled && isAFrameSupported) {
-      console.log('All conditions met. Applying styles for "yes" elements.');
-      // Apply styles for "yes" elements
-      applyStyles(yesAfHideElements, 'hidden', '');
-      applyStyles(yesAfRemoveElements, 'visible', 'none');
-    } else {
-      console.log('Conditions not met. Applying styles for "no" elements.');
-      // Apply styles for "no" elements
-      applyStyles(noAfHideElements, 'hidden', '');
-      applyStyles(noAfRemoveElements, 'visible', 'none');
-    }
-  }
-
-  // Execute the check and apply styles
-  checkAndApplyStyles();
+  // Call the function when the DOM content has finished loading
+  applyStyles();
 });
