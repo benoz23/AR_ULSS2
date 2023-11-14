baf83535acfe68ff3f2c9edbf8cf99b17214019c
benoz23 <gianluca.benotto@gmail.com>
2023-11-14 19:56:00 +0100

    js fix

--- main.js
+++ main.js
@@ -1,51 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var elementsToHide = [];
-  var elementsToRemove = [];
+  // Check for A-Frame support
+  var afSupported = typeof AFRAME !== 'undefined' && document.querySelector('a-scene');
 
-  // Check if Device Motion API is supported
-  if ('ondevicemotion' in window) {
-    // Device Motion API is supported
-    console.log('Device Motion API supported');
+  // Check for Device Motion API support
+  var motionSensorsEnabled = 'ondevicemotion' in window;
 
-    // Check if the device has motion sensors enabled
-    window.addEventListener('devicemotion', function(event) {
-      if (event.accelerationIncludingGravity !== null) {
-        // Motion sensors are enabled
+  if (motionSensorsEnabled) {
+    // Listen for motion events to check if motion sensors are enabled
+    window.addEventListener('devicemotion', function (event) {
+      motionSensorsEnabled = event.accelerationIncludingGravity !== null;
+      if (motionSensorsEnabled) {
         console.log('Motion sensors enabled');
-
-        // Check if Device Orientation API is supported
-        if ('ondeviceorientation' in window) {
-          // Device Orientation API is supported
-          console.log('Device Orientation API supported');
-
-          // Check if the device has orientation sensors enabled
-          window.addEventListener('deviceorientation', function(event) {
-            if (event.alpha !== null || event.beta !== null || event.gamma !== null) {
-              // Orientation sensors are enabled
-              console.log('Orientation sensors enabled');
-
-              // Check if A-Frame is supported
-              if (typeof AFRAME !== 'undefined') {
-                // A-Frame is supported
-                elementsToHide = document.querySelectorAll('[yes_af_hide]');
-                elementsToRemove = document.querySelectorAll('[yes_af_remove]');
-              }
-            }
-          });
-        }
       }
     });
+  } else {
+    console.error('Error: Device Motion API is not supported');
   }
 
-  // If any condition fails, set elements for no A-Frame support
-  if (elementsToHide.length === 0 && elementsToRemove.length === 0) {
-    elementsToHide = document.querySelectorAll('[no_af_hide]');
-    elementsToRemove = document.querySelectorAll('[no_af_remove]');
+  // Check for Device Orientation API support
+  var orientationSensorsEnabled = motionSensorsEnabled && 'ondeviceorientation' in window;
+
+  if (orientationSensorsEnabled) {
+    // Listen for orientation events to check if orientation sensors are enabled
+    window.addEventListener('deviceorientation', function (event) {
+      orientationSensorsEnabled = event.alpha !== null || event.beta !== null || event.gamma !== null;
+      if (orientationSensorsEnabled) {
+        console.log('Orientation sensors enabled');
+      }
+    });
+  } else {
+    console.error('Error: Device Orientation API is not supported');
   }
+
+  // Select elements to hide or remove based on feature support
+  var elementsToHide = afSupported && orientationSensorsEnabled ? document.querySelectorAll('[yes_af_hide]') : document.querySelectorAll('[no_af_hide]');
+  var elementsToRemove = afSupported && orientationSensorsEnabled ? document.querySelectorAll('[yes_af_remove]') : document.querySelectorAll('[no_af_remove]');
 
   // Apply CSS styles to elements with "visibility: hidden"
   elementsToHide.forEach(function (element) {
-    element.style.visibility = 'hidden';
+    element.classList.add('hidden');
   });
 
   // Apply CSS styles to elements with "display: none"
