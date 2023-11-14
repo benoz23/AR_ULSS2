1696febc66f2d9f57f593ee7ef45ee6cae2addf1
benoz23 <gianluca.benotto@gmail.com>
2023-11-14 22:56:43 +0100

    js reset

--- main.js
+++ main.js
@@ -1,50 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('Checking for sensors and A-Frame support');
-
-  // Check if sensors are present and enabled
-  const hasSensors = navigator.sensors && navigator.sensors.enabled;
-  console.log('Sensors present and enabled:', hasSensors);
-
-  // Check if A-Frame is supported
-  const hasAFrame = typeof AFrame !== 'undefined';
-  console.log('A-Frame supported:', hasAFrame);
-
-  // Apply visibility or display styles based on the conditions
-  const yesAfHideElements = document.querySelectorAll('[yes_af_hide]');
-  console.log('Number of elements with yes_af_hide attribute:', yesAfHideElements.length);
-
-  const yesAfRemoveElements = document.querySelectorAll('[yes_af_remove]');
-  console.log('Number of elements with yes_af_remove attribute:', yesAfRemoveElements.length);
-
-  const noAfHideElements = document.querySelectorAll('[no_af_hide]');
-  console.log('Number of elements with no_af_hide attribute:', noAfHideElements.length);
-
-  const noAfRemoveElements = document.querySelectorAll('[no_af_remove]');
-  console.log('Number of elements with no_af_remove attribute:', noAfRemoveElements.length);
-
-  if (hasSensors && hasAFrame) {
-    console.log('Applying visibility or display styles for A-Frame enabled scenario');
-
-    for (const element of yesAfHideElements) {
+document.addEventListener('DOMContentLoaded', function () {
+    var elementsToHide = [];
+    var elementsToRemove = [];
+  
+    if (typeof AFRAME === 'undefined') {
+      // A-Frame is not supported
+      elementsToHide = document.querySelectorAll('[no_af_hide]');
+      elementsToRemove = document.querySelectorAll('[no_af_remove]');
+    } else {
+      // A-Frame is supported
+      elementsToHide = document.querySelectorAll('[yes_af_hide]');
+      elementsToRemove = document.querySelectorAll('[yes_af_remove]');
+    }
+  
+    // Apply CSS styles to elements with "visibility: hidden"
+    elementsToHide.forEach(function (element) {
       element.style.visibility = 'hidden';
-      console.log('Applying visibility: hidden to element:', element);
-    }
-
-    for (const element of yesAfRemoveElements) {
+    });
+  
+    // Apply CSS styles to elements with "display: none"
+    elementsToRemove.forEach(function (element) {
       element.style.display = 'none';
-      console.log('Applying display: none to element:', element);
-    }
-  } else {
-    console.log('Applying visibility or display styles for A-Frame fallback');
-
-    for (const element of noAfHideElements) {
-      element.style.visibility = 'hidden';
-      console.log('Applying visibility: hidden to element:', element);
-    }
-
-    for (const element of noAfRemoveElements) {
-      element.style.display = 'none';
-      console.log('Applying display: none to element:', element);
-    }
-  }
-});
+    });
+  });
+  
