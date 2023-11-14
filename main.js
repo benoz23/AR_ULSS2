07f5df9b19d27d09b430218144b999a9b8ee8e92
benoz23 <gianluca.benotto@gmail.com>
2023-11-14 22:42:16 +0100

    ja fix

--- main.js
+++ main.js
@@ -23,14 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const aFrameSupported = typeof AFRAME !== "undefined";
   console.log("A-frame supported:", aFrameSupported);
 
-  if (sensorsEnabled && aFrameSupported) {
-      // Apply styles to elements with attributes "yes_af_hide" and "yes_af_remove"
-      applyStyles("[yes_af_hide]", "visibility: hidden");
-      applyStyles("[yes_af_remove]", "display: none");
-  } else {
+  if (!sensorsEnabled || !aFrameSupported) {
       // Apply styles to elements with attributes "no_af_hide" and "no_af_remove"
       applyStyles("[no_af_hide]", "visibility: hidden");
       applyStyles("[no_af_remove]", "display: none");
+  } else {
+      // Apply styles to elements with attributes "yes_af_hide" and "yes_af_remove"
+      applyStyles("[yes_af_hide]", "visibility: hidden");
+      applyStyles("[yes_af_remove]", "display: none");
   }
 });
 
