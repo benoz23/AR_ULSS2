934caab80beb3e2aaf5bc2176509de6ad004b507
benoz23 <gianluca.benotto@gmail.com>
2023-11-14 20:21:20 +0100

    js fix

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Check for both Device Motion and Orientation APIs
-  const deviceMotionSupported = 'ondevicemotion' in window;
-  const deviceOrientationSupported = 'ondeviceorientation' in window;
+  // Check for device motion sensors
+  const hasMotionSensors = 'ondevicemotion' in window;
 
-  // Common logic for handling A-Frame or non-A-Frame scenarios
-  function handleDeviceCapabilities(isAFramePresent) {
-    const hideSelector = isAFramePresent ? '[yes_af_hide]' : '[no_af_hide]';
-    const removeSelector = isAFramePresent ? '[yes_af_remove]' : '[no_af_remove]';
-
-    const hideElements = document.querySelectorAll(hideSelector);
-    const removeElements = document.querySelectorAll(removeSelector);
-
-    hideElements.forEach(element => element.style.visibility = 'hidden');
-    removeElements.forEach(element => element.style.display = 'none');
-  }
-
-  // Handle device motion API
-  if (deviceMotionSupported) {
+  if (hasMotionSensors) {
+    // Attach event listener for device motion
     window.addEventListener('devicemotion', function (event) {
       if (event.accelerationIncludingGravity) {
         console.log('Motion sensors enabled');
@@ -27,12 +14,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   } else {
-    console.log('Device Motion API not supported');
+    console.log('Device Motion API not supported or no motion sensors detected');
     handleDeviceCapabilities(false);
   }
 
-  // Handle device orientation API
-  if (deviceOrientationSupported) {
+  // Check for device orientation sensors
+  const hasOrientationSensors = 'ondeviceorientation' in window;
+
+  if (hasOrientationSensors) {
+    // Attach event listener for device orientation
     window.addEventListener('deviceorientation', function (event) {
       if (event.alpha !== null || event.beta !== null || event.gamma !== null) {
         console.log('Orientation sensors enabled');
@@ -43,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   } else {
-    console.log('Device Orientation API not supported');
+    console.log('Device Orientation API not supported or no orientation sensors detected');
     handleDeviceCapabilities(false);
   }
 });
