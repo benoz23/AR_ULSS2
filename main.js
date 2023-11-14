ecc0e4a5e754fe29a3ff3e9003fb0adfd4d9a135
benoz23 <gianluca.benotto@gmail.com>
2023-11-14 23:01:40 +0100

    js fix

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elementsToHide = [];
-    var elementsToRemove = [];
-  
-    if (typeof AFRAME === 'undefined') {
-      // A-Frame is not supported
-      elementsToHide = document.querySelectorAll('[no_af_hide]');
-      elementsToRemove = document.querySelectorAll('[no_af_remove]');
-    } else {
-      // A-Frame is supported
-      elementsToHide = document.querySelectorAll('[yes_af_hide]');
-      elementsToRemove = document.querySelectorAll('[yes_af_remove]');
-    }
-  
-    // Apply CSS styles to elements with "visibility: hidden"
-    elementsToHide.forEach(function (element) {
-      element.style.visibility = 'hidden';
-    });
-  
-    // Apply CSS styles to elements with "display: none"
-    elementsToRemove.forEach(function (element) {
-      element.style.display = 'none';
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Check if sensors are present and enabled
+  let sensorsEnabled = false;
+
+  if ('DeviceMotionEvent' in window) {
+      window.addEventListener("devicemotion", function (event) {
+          if (event.rotationRate && (event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma)) {
+              sensorsEnabled = true;
+          }
+      });
+  } else {
+      console.log("DeviceMotionEvent is not supported on this device.");
+  }
+
+  // Check if A-frame is supported
+  const aFrameSupported = typeof AFRAME !== "undefined";
+  console.log("A-frame supported:", aFrameSupported);
+
+  if (!sensorsEnabled || !aFrameSupported) {
+      // Apply styles to elements with attributes "no_af_hide" and "no_af_remove"
+      applyStyles("[no_af_hide]", "visibility: hidden");
+      applyStyles("[no_af_remove]", "display: none");
+  } else {
+      // Apply styles to elements with attributes "yes_af_hide" and "yes_af_remove"
+      applyStyles("[yes_af_hide]", "visibility: hidden");
+      applyStyles("[yes_af_remove]", "display: none");
+  }
+});
+
+function applyStyles(selector, styles) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach(function (element) {
+      element.style.cssText += ";" + styles;
   });
-  
+}
