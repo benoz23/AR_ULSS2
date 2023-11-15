61c09fc6cc91deb0a2ad768f7a12d54ee465620b
benoz23 <gianluca.benotto@gmail.com>
2023-11-15 12:38:52 +0100

    js fix

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("A-frame supported:", aFrameSupported);
 
     let sensorsEnabled = false;
-    let stylesApplied = false;
 
     function handleDeviceMotionEvent(event) {
         console.log("DeviceMotionEvent received:", event);
@@ -21,47 +20,36 @@ document.addEventListener("DOMContentLoaded", function () {
                 event.rotationRate.gamma !== 0
             );
 
-            console.log("Has valid rotationRate:", hasValidRotationRate);
-
             if (hasValidRotationRate) {
                 sensorsEnabled = true;
                 console.log("Gyroscope present.");
 
                 // Check if A-frame is supported after gyroscope detection
                 if (!aFrameSupported || !sensorsEnabled) {
-                    if (!stylesApplied) {
-                        // Apply styles to elements with class "if-no-sup-hide" and "if-no-sup-remove"
-                        applyStyles(".if-no-sup-hide", "visibility: hidden");
-                        applyStyles(".if-no-sup-remove", "display: none");
-                        applyStyles(".if-sup-hide", "visibility: visible");
-                        applyStyles(".if-sup-remove", "display: block");
-                        console.log("Fallback: Showing elements with class 'if-no-sup-remove' and hiding 'if-no-sup-hide'.");
-                        stylesApplied = true;
-                    }
+                    // Apply styles to elements with class "if-no-sup-hide" and "if-no-sup-remove"
+                    applyStyles(".if-no-sup-hide", "visibility: hidden");
+                    applyStyles(".if-no-sup-remove", "display: none");
+                    applyStyles(".if-sup-hide", "visibility: visible");
+                    applyStyles(".if-sup-remove", "display: block");
+                    console.log("Fallback: Showing elements with class 'if-no-sup-remove' and hiding 'if-no-sup-hide'.");
                 } else {
-                    if (!stylesApplied) {
-                        // Apply styles to elements with class "if-sup-hide" and "if-sup-remove"
-                        applyStyles(".if-sup-hide", "visibility: hidden");
-                        applyStyles(".if-sup-remove", "display: none");
-                        applyStyles(".if-no-sup-hide", "visibility: visible");
-                        applyStyles(".if-no-sup-remove", "display: block");
-                        console.log("Applying styles for 'if-sup-hide' and 'if-sup-remove'.");
-                        stylesApplied = true;
-                    }
+                    // Apply styles to elements with class "if-sup-hide" and "if-sup-remove"
+                    applyStyles(".if-sup-hide", "visibility: hidden");
+                    applyStyles(".if-sup-remove", "display: none");
+                    applyStyles(".if-no-sup-hide", "visibility: visible");
+                    applyStyles(".if-no-sup-remove", "display: block");
+                    console.log("Applying styles for 'if-sup-hide' and 'if-sup-remove'.");
                 }
             } else {
                 // Gyroscope data is not present or all values are null, undefined, or zero
                 console.log("Gyroscope not present or invalid rotationRate values.");
 
                 // Apply styles to elements with class "if-no-sup-hide" and "if-no-sup-remove"
-                if (!stylesApplied) {
-                    applyStyles(".if-no-sup-hide", "visibility: hidden");
-                    applyStyles(".if-no-sup-remove", "display: none");
-                    applyStyles(".if-sup-hide", "visibility: visible");
-                    applyStyles(".if-sup-remove", "display: block");
-                    console.log("Fallback: Showing elements with class 'if-no-sup-remove' and hiding 'if-no-sup-hide'.");
-                    stylesApplied = true;
-                }
+                applyStyles(".if-no-sup-hide", "visibility: hidden");
+                applyStyles(".if-no-sup-remove", "display: none");
+                applyStyles(".if-sup-hide", "visibility: visible");
+                applyStyles(".if-sup-remove", "display: block");
+                console.log("Fallback: Showing elements with class 'if-no-sup-remove' and hiding 'if-no-sup-hide'.");
             }
 
             // Remove the event listener after handling the event
@@ -75,14 +63,11 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("DeviceMotionEvent is not supported on this device.");
 
         // Apply styles to elements with class "if-no-sup-hide" and "if-no-sup-remove" if sensors are not supported
-        if (!stylesApplied) {
-            applyStyles(".if-no-sup-hide", "visibility: hidden");
-            applyStyles(".if-no-sup-remove", "display: none");
-            applyStyles(".if-sup-hide", "visibility: visible");
-            applyStyles(".if-sup-remove", "display: block");
-            console.log("Fallback: Showing elements with class 'if-no-sup-remove' and hiding 'if-no-sup-hide'.");
-            stylesApplied = true;
-        }
+        applyStyles(".if-no-sup-hide", "visibility: hidden");
+        applyStyles(".if-no-sup-remove", "display: none");
+        applyStyles(".if-sup-hide", "visibility: visible");
+        applyStyles(".if-sup-remove", "display: block");
+        console.log("Fallback: Showing elements with class 'if-no-sup-remove' and hiding 'if-no-sup-hide'.");
     }
 });
 
