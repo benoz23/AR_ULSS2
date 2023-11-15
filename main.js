d47c823be8670bb2d832900d680247e104141fd4
benoz23 <gianluca.benotto@gmail.com>
2023-11-15 07:24:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,14 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 // Check if A-frame is supported after gyroscope detection
                 if (!aFrameSupported || !sensorsEnabled) {
-                    // Apply styles to elements with attributes "no_af_hide" and "no_af_remove"
-                    applyStyles("[no_af_hide]", "visibility: hidden");
-                    applyStyles("[no_af_remove]", "display: none");
-                    console.log("Fallback: Showing elements with attributes 'no_af_remove' and hiding 'no_af_hide'.");
+                    // Apply styles to elements with class "no_af_hide" and "no_af_remove"
+                    applyStyles(".no_af_hide", "visibility: hidden");
+                    applyStyles(".no_af_remove", "display: none");
+                    applyStyles(".yes_af_hide", "visibility: visible");
+                    applyStyles(".yes_af_remove", "display: block");
+                    console.log("Fallback: Showing elements with class 'no_af_remove' and hiding 'no_af_hide'.");
                 } else {
-                    // Apply styles to elements with attributes "yes_af_hide" and "yes_af_remove"
-                    applyStyles("[yes_af_hide]", "visibility: hidden");
-                    applyStyles("[yes_af_remove]", "display: none");
+                    // Apply styles to elements with class "yes_af_hide" and "yes_af_remove"
+                    applyStyles(".yes_af_hide", "visibility: hidden");
+                    applyStyles(".yes_af_remove", "display: none");
+                    applyStyles(".no_af_hide", "visibility: visible");
+                    applyStyles(".no_af_remove", "display: block");
                     console.log("Applying styles for 'yes_af_hide' and 'yes_af_remove'.");
                 }
             }
@@ -29,10 +33,12 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
         console.log("DeviceMotionEvent is not supported on this device.");
 
-        // Apply styles to elements with attributes "no_af_hide" and "no_af_remove" if sensors are not supported
-        applyStyles("[no_af_hide]", "visibility: hidden");
-        applyStyles("[no_af_remove]", "display: none");
-        console.log("Fallback: Showing elements with attributes 'no_af_remove' and hiding 'no_af_hide'.");
+        // Apply styles to elements with class "no_af_hide" and "no_af_remove" if sensors are not supported
+        applyStyles(".no_af_hide", "visibility: hidden");
+        applyStyles(".no_af_remove", "display: none");
+        applyStyles(".yes_af_hide", "visibility: visible");
+        applyStyles(".yes_af_remove", "display: block");
+        console.log("Fallback: Showing elements with class 'no_af_remove' and hiding 'no_af_hide'.");
     }
 });
 
