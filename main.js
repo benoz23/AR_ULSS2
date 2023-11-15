4b0f07b196c271e38a1e09e22194b81de0608c84
benoz23 <gianluca.benotto@gmail.com>
2023-11-15 07:32:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,17 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
                 // Check if A-frame is supported after gyroscope detection
                 if (!aFrameSupported || !sensorsEnabled) {
                     // Apply styles to elements with class "no_af_hide" and "no_af_remove"
-                    applyStyles(".no_af_hide", "visibility: hidden");
-                    applyStyles(".no_af_remove", "display: none");
-                    applyStyles(".yes_af_hide", "visibility: visible");
-                    applyStyles(".yes_af_remove", "display: block");
+                    applyStyles(".if-no-sup-hide", "visibility: hidden");
+                    applyStyles(".if-no-sup-remove", "display: none");
+                    applyStyles(".if-sup-hide", "visibility: visible");
+                    applyStyles(".if-sup-remove", "display: block");
                     console.log("Fallback: Showing elements with class 'no_af_remove' and hiding 'no_af_hide'.");
                 } else {
                     // Apply styles to elements with class "yes_af_hide" and "yes_af_remove"
-                    applyStyles(".yes_af_hide", "visibility: hidden");
-                    applyStyles(".yes_af_remove", "display: none");
-                    applyStyles(".no_af_hide", "visibility: visible");
-                    applyStyles(".no_af_remove", "display: block");
+                    applyStyles(".if-sup-hide", "visibility: hidden");
+                    applyStyles(".if-sup-remove", "display: none");
+                    applyStyles(".if-no-sup-hide", "visibility: visible");
+                    applyStyles(".if-no-sup-remove", "display: block");
                     console.log("Applying styles for 'yes_af_hide' and 'yes_af_remove'.");
                 }
             }
@@ -34,10 +34,10 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("DeviceMotionEvent is not supported on this device.");
 
         // Apply styles to elements with class "no_af_hide" and "no_af_remove" if sensors are not supported
-        applyStyles(".no_af_hide", "visibility: hidden");
-        applyStyles(".no_af_remove", "display: none");
-        applyStyles(".yes_af_hide", "visibility: visible");
-        applyStyles(".yes_af_remove", "display: block");
+        applyStyles(".if-no-sup-hide", "visibility: hidden");
+        applyStyles(".if-no-sup-remove", "display: none");
+        applyStyles(".if-sup-hide", "visibility: visible");
+        applyStyles(".if-sup-remove", "display: block");
         console.log("Fallback: Showing elements with class 'no_af_remove' and hiding 'no_af_hide'.");
     }
 });
